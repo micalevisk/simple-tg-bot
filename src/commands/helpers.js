746742994fae04f19e657f261b3b2ed0113d1883
Micael Levi <mllc@icomp.ufam.edu.br>
2020-11-19 14:17:18 -0400

    Auto import commands modules

--- src/commands/helpers.js
+++ src/commands/helpers.js
@@ -1,3 +1,6 @@
+const fs = require('fs')
+const path = require('path')
+
 /**
  * Interceptor middleware to check wheter or not it should call the next
  * middleware in the chain when the last middleware fail when doing its task.
@@ -18,5 +21,28 @@ module.exports.continueOnLastMiddlewareError = (ignoreErrors = true) => {
       }
     }
     next()
+  }
+}
+
+/**
+ * Walk through a directory recursively (in a depth-first manner).
+ * (c) {@link https://gist.github.com/lovasoa/8691344}
+ * @param {string} dirPath
+ */
+module.exports.walkSync = function* walkSync(
+  dirPath,
+  includeFile = (filePath) => true,
+) {
+  const files = fs.readdirSync(dirPath)
+  for (const file of files) {
+    const filepath = path.join(dirPath, file)
+    const fileStats = fs.statSync(filepath)
+    if (fileStats.isDirectory()) {
+      yield* walkSync(filepath, includeFile)
+    } else {
+      if (includeFile(filepath)) {
+        yield filepath
+      }
+    }
   }
 }
--- src/commands/index.js
+++ src/commands/index.js
@@ -1,1 +1,12 @@
-module.exports = [require('./vaga'), require('./radmins')]
+const { walkSync } = require('./helpers')
+
+module.exports = (function importCommands() {
+  /** @param {string} filepath */
+  const isCommandModuleFile = (filepath) => filepath.endsWith('.command.js')
+
+  const commands = Array.from(
+    walkSync(__dirname, isCommandModuleFile),
+  ).map((commandModuleAbsFile) => require(commandModuleAbsFile))
+
+  return commands
+})()
